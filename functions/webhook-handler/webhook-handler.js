44b69aa6b01772d824119a7fde255629c8382ead
Jon Henningsson <jon.henningsson@gmail.com>
2020-05-11 11:05:38 -0500

    .

--- functions/webhook-handler/webhook-handler.js
+++ functions/webhook-handler/webhook-handler.js
@@ -2,35 +2,47 @@ const mystrava = require('mystrava');
 
 exports.handler = async (event, context) => {
   try {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-    const mys = new mystrava();
-
     // default response data
-    var statusCode = 200;
-    var body;
+    var response = {
+      statusCode = 200;
+    }
 
     var verify_token_req = event.queryStringParameters["hub.verify_token"];
+    var challenge = event.queryStringParameters["hub.challenge"];
 
+    const mys = new mystrava();
     if (mys.is_validation_req(event)) {
+
       if (verify_token_req == mys.verify_token) {
         console.log("Valid validation request!");
+        response.headers: {
+            "Content-Type": "application/json"
+          },
+          response.body: JSON.stringify({
+            "hub.challenge": challenge
+          });
+      } else {
+        throw "Verify token incorrect";
       }
 
+    } else {
+      console.log("Not a validation request!");
     }
 
+    // success response
+    return response
 
+    // failure response
+  } catch (err) {
+    console.log(err);
     return {
-      statusCode: statusCode,
-      headers: {"Content-Type":"application/json"},
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({
-        "status": "success",
-        "message": statusMessage
+        "status": "error"
       })
     }
-
-  } catch (err) {
-
-    console.log(err);
-    return { statusCode: 500, body: err.toString() }
   }
 }
